9181a798d064f55bbeb2b0aea4272d19ea5028c5
LeKoArts <lekoarts@gmail.com>
2018-03-13 20:54:26 +0100

    Updated post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,20 +1,77 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
-import styles from './post.module.css';
+import styled from 'styled-components';
+import { darken, lighten } from 'polished';
+import Wrapper from '../components/Wrapper';
+import Subline from '../components/Subline';
+import { media } from '../utils/media';
+
+const Content = styled.article`
+  grid-column: 2;
+  box-shadow: 0 4px 120px rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+  padding: 2rem 4rem;
+  background-color: ${props => props.theme.bg};
+  z-index: 9000;
+  margin-top: -3rem;
+  @media ${media.tablet} {
+    padding: 3rem 3rem;
+  }
+  @media ${media.phone} {
+    padding: 2rem 1.5rem;
+  }
+`;
+
+const Header = styled.header`
+  background: linear-gradient(
+    45deg,
+    ${props => darken(0.1, props.theme.primary)},
+    ${props => lighten(0.1, props.theme.primary)}
+  );
+  grid-column: 1 / -1;
+  margin-left: -1rem;
+  margin-right: -1rem;
+  padding: 2rem 2rem 5rem 2rem;
+  box-shadow: inset 0px -10px 30px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const HeaderContent = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  a {
+    color: white;
+  }
+`;
+
+const Title = styled.h1`
+  margin-top: 2rem;
+`;
+
+const PostContent = styled.div`
+  margin-top: 4rem;
+`;
 
 const Post = props => {
   const postNode = props.data.markdownRemark;
   const post = postNode.frontmatter;
 
   return (
-    <article className={styles.blogPost}>
+    <Wrapper>
       <Helmet title={`${post.title} | Gatsby Starter - Minimal Blog`} />
-      <Link to="/">Gatsby Starter - Minimal Blog | Get back to the overview</Link>
-      <h1 className={styles.title}>{post.title}</h1>
-      <h4 className={styles.date}>{post.date}</h4>
-      <div className={styles.content} dangerouslySetInnerHTML={{ __html: postNode.html }} />
-    </article>
+      <Header>
+        <HeaderContent>
+          <Link to="/">Gatsby Starter - Minimal Blog | Get back to the overview</Link>
+        </HeaderContent>
+      </Header>
+      <Content>
+        <Title>{post.title}</Title>
+        <Subline>
+          {post.date} &mdash; {postNode.timeToRead} Min Read
+        </Subline>
+        <PostContent dangerouslySetInnerHTML={{ __html: postNode.html }} />
+      </Content>
+    </Wrapper>
   );
 };
 
@@ -29,6 +86,7 @@ export const postQuery = graphql`
         title
         date(formatString: "DD.MM.YYYY")
       }
+      timeToRead
     }
   }
 `;
